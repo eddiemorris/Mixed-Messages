67c1f526daaf69354705a3a44d594b1b1c3b2975
Eddie Bingo <102780046+eddiemorris@users.noreply.github.com>
2023-10-03 22:24:56 -0400

    First iteration of chord picker

---
+++ main.js
@@ -0,0 +1,26 @@
+let playLine = ["bouncy", "down", "up"];
+
+function key() {
+    const scaleKey = (Math.floor(Math.random()*2))+1;
+    return scaleKey;
+}
+
+let randomDegree;
+function firstChord() {
+    const first = (Math.floor(Math.random()*7))+1
+    return first;
+};
+let secondChord;
+let thirdChord;
+let lastChord;
+let finalProg = [];
+
+function fullPicker() {
+
+};
+
+
+console.log(firstChord());
+
+
+
